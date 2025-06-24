a342093c666e1b07a13843505e45dc2a658be15f
ryanwisemanmusic <155513131+ryanwisemanmusic@users.noreply.github.com>
2025-06-24 12:47:08 -0700

    ARM64 Wine V0.01
    Character declarations and storage sizes created, need some reworking on reserved characters so they can be used in an assembly process

---
+++ src/storageTypes.h
@@ -0,0 +1,17 @@
+#include <cstdint>
+
+// Standard approach - recommended
+//Byte ARM equivalent
+typedef uint8_t byte;
+//Word ARM equivalent
+typedef uint16_t word;
+//Double-Word ARM equivalent
+typedef uint32_t dword;
+//Quad-Word ARM equivalent
+typedef uint64_t qword;
+//Double-Quad-Word ARM equivalent
+typedef uint64_t dqword;
+
+
+
+
